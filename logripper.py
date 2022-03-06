f521c12092cb5bb91a95da9d259c065a67335c18
Chris <cgrabda@gmail.com>
2022-03-05 18:15:57 -0800

    fixed sshStats output and actionSsh message

--- logripper.py
+++ logripper.py
@@ -13,10 +13,7 @@ import time
 ID_NUM = 0
 TOKEN = ''
 
-#AUTHLOG = "/var/log/auth.log"
-AUTHLOG = "/home/csec202/auth.log"
-
-RE_SSHD = re.compile(r".*sshd.*Failed.*")
+AUTHLOG = "/var/log/auth.log"
 
 
 def parseAuth():
@@ -29,7 +26,7 @@ def parseAuth():
     with open(AUTHLOG) as file:
         # goes through each line of the auth file and checks for failed ssh attempts
         for line in file:
-            if (RE_SSHD.search(line)):
+            if (re.compile(r".*sshd.*Failed.*").search(line)):
                 # 
                 tokens = line.strip().split(' ')
                 date = line[:15]
@@ -61,18 +58,21 @@ def sshStats(sshList):
             nameDict[entry[1]] = 1
             ipDict[entry[2]] = 1
 
-    return [ipDict, nameDict]
+    output = {"ip":ipDict, "user":nameDict}
+    return output
 
 def post(command, data):
     '''
     sends information to server
+    if action is within the response, e.g. report is called, the requested information is sent
 
     commands:
     agentUpdate {id: number, agentToken: string, cpu: number, memory: number, netIn: number, netOut: number, disk: number}
+    actionSss
     '''
     response = requests.post('test/api/' + command, data)
     if (json.loads(response)["action"] == "ssh"):
-        data = parseAuth()
+        data = sshStats(parseAuth()[0])
         post('actionSsh', json.dumps(data))
 
 
